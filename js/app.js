f25b5af722d3bebf56fee1f86074e96408943197
granitaistore-prog <granit.ai.store@gmail.com>
2026-01-20 12:03:33 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,16 +2,18 @@ if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("service-worker.js");
 }
 
-const map = L.map("map").setView([48.3794, 31.1656], 6);
+document.addEventListener("DOMContentLoaded", () => {
+  window.map = L.map("map").setView([48.3794, 31.1656], 6);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 18
-}).addTo(map);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 18
+  }).addTo(map);
 
-loadRegions();
-
-// автооновлення кожні 2 хв
-setInterval(() => {
-  if (regionsLayer) map.removeLayer(regionsLayer);
   loadRegions();
-}, 120000);
+
+  // автооновлення кожні 2 хв
+  setInterval(() => {
+    if (window.regionsLayer) map.removeLayer(regionsLayer);
+    loadRegions();
+  }, 120000);
+});

13242c8824e7f28a9273412680c65521af993228
granitaistore-prog <granit.ai.store@gmail.com>
2026-01-20 13:29:28 +0200

    Update api-integration.js

--- js/api-integration.js
+++ js/api-integration.js
@@ -1,3 +1,10 @@
+// Дані по всій Україні (поки з локального JSON)
+async function loadOutageData() {
+  const r = await fetch("data/status.json");
+  return await r.json();
+}
+
+// Дані по твоїй адресі (YASNO)
 async function loadUserQueue() {
   const r = await fetch("https://svitlo-ye-api.granit-ai-store.workers.dev/?city=baranivka&street=petliury&house=25");
   return await r.json();

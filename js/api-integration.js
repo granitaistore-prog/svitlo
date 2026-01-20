23091a328bb9678344169de9cff6e0ca3eb6c88b
granitaistore-prog <granit.ai.store@gmail.com>
2026-01-20 12:51:58 +0200

    Update api-integration.js

--- js/api-integration.js
+++ js/api-integration.js
@@ -1,4 +1,4 @@
 async function loadOutageData() {
   const r = await fetch("https://svitlo-ye-api.granit-ai-store.workers.dev/");
-  return await r.json(); // { "UA-01": {...}, "UA-02": {...} }
+  return await r.json(); // ключі = ISO області
 }

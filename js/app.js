08b56a6d5c9835dc3d47afa05423015771fb9dcd
granitaistore-prog <granit.ai.store@gmail.com>
2026-01-20 11:24:43 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,13 +2,10 @@ if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("service-worker.js");
 }
 
-document.getElementById("status").innerText = "Карта України завантажується...";
-
 const map = L.map("map").setView([48.3794, 31.1656], 6);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  maxZoom: 18,
-  attribution: "© OpenStreetMap"
+  maxZoom: 18
 }).addTo(map);
 
-document.getElementById("status").innerText = "Початкові дані: карта готова";
+loadRegions();

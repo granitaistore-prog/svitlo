51d3a37e434a5ca608de0e40bc3981b2af448a85
granitaistore-prog <granit.ai.store@gmail.com>
2026-01-20 12:48:06 +0200

    Update api-integration.js

--- js/api-integration.js
+++ js/api-integration.js
@@ -1,6 +1,4 @@
 async function loadOutageData() {
-  const r = await fetch("https://svitlo-ye-api.granit-ai-store.workers.dev/?city=Баранівка&street=Симона%20Петлюри&house=25");
-  const d = await r.json();
-
-  return d; // ключ = ISO області, значення = статус
+  const r = await fetch("https://svitlo-ye-api.granit-ai-store.workers.dev/");
+  return await r.json(); // { "UA-01": {...}, "UA-02": {...} }
 }
